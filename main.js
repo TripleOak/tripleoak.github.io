2cecbfabf038cf01835fa6db1bd89cd4fae6e3b5
jpldcarvalho <jpldcarvalho@gmail.com>
2021-02-25 21:29:41 +0000

    restructuring and text in homepage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
     var bar = document.querySelector("nav");
-    if(this.scrollY > window.innerHeight / 2 - bar.clientHeight) {
+    if(this.scrollY > window.innerHeight / 8 - bar.clientHeight) {
         bar.style.backgroundColor = '#16202a';
         bar.style.border = 'none';
     } else {
